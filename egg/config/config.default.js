de8a8da9107ec786082b82f10d3def6eaaae9088
liuyuhui0111 <84964308@qq.com>
2019-12-15 12:22:50 +0800

    数据库密码配置

--- egg/config/config.default.js
+++ egg/config/config.default.js
@@ -36,7 +36,7 @@ module.exports = appInfo => {
         // 用户名
         user: 'root',
         // 密码
-        password: 'root',
+        password: 'Lyh536325.',
         // 数据库名
         database: 'test',
       },
---
+++ vue/public/dialog-demo.js
@@ -0,0 +1,12 @@
+;(function(window,$){
+    $.fn.dialog = function(options){
+        if(!$(this).data("dialog")){
+            $(this).data("dialog",this);
+            $(body).append()
+        }else{
+            console.log(222)
+        }
+    }
+
+    var _createDom
+})(window,$);
